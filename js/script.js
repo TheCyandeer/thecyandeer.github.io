5c1394c2350d0c029610140dea0f0471231deae1
thecyandeer <thecyandeer@163.com>
2026-02-22 02:29:03 +0800

    添加了视频合集页面，并更新了赞助页面

--- js/script.js
+++ js/script.js
@@ -48,3 +48,97 @@
         });
     }
 })();
+
+// Video Accordion Animation
+(function() {
+    const accordions = []; // Store all accordion instances
+
+    class Accordion {
+        constructor(el) {
+            this.el = el;
+            this.summary = el.querySelector('summary');
+            this.content = el.querySelector('.video-content');
+            this.animation = null;
+            this.isClosing = false;
+            this.isExpanding = false;
+            this.summary.addEventListener('click', (e) => this.onClick(e));
+            accordions.push(this); // Add this instance to the list
+        }
+
+        onClick(e) {
+            e.preventDefault();
+            this.el.style.overflow = 'hidden';
+            if (this.isClosing || !this.el.open) {
+                // Close other open accordions
+                accordions.forEach(acc => {
+                    if (acc !== this && (acc.el.open || acc.isExpanding)) {
+                        acc.shrink();
+                    }
+                });
+                this.open();
+            } else if (this.isExpanding || this.el.open) {
+                this.shrink();
+            }
+        }
+
+        open() {
+            this.el.style.height = `${this.el.offsetHeight}px`;
+            this.el.open = true;
+            window.requestAnimationFrame(() => this.expand());
+        }
+
+        expand() {
+            this.isExpanding = true;
+            const startHeight = `${this.el.offsetHeight}px`;
+            const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+            
+            if (this.animation) {
+                this.animation.cancel();
+            }
+            
+            this.animation = this.el.animate({
+                height: [startHeight, endHeight]
+            }, {
+                duration: 400,
+                easing: 'ease-out'
+            });
+
+            this.animation.onfinish = () => this.onAnimationFinish(true);
+            this.animation.oncancel = () => this.isExpanding = false;
+        }
+
+        shrink() {
+            this.isClosing = true;
+            const startHeight = `${this.el.offsetHeight}px`;
+            const endHeight = `${this.summary.offsetHeight}px`;
+
+            if (this.animation) {
+                this.animation.cancel();
+            }
+
+            this.animation = this.el.animate({
+                height: [startHeight, endHeight]
+            }, {
+                duration: 400,
+                easing: 'ease-out'
+            });
+
+            this.animation.onfinish = () => this.onAnimationFinish(false);
+            this.animation.oncancel = () => this.isClosing = false;
+        }
+
+        onAnimationFinish(open) {
+            this.el.open = open;
+            this.animation = null;
+            this.isClosing = false;
+            this.isExpanding = false;
+            this.el.style.height = '';
+            this.el.style.overflow = '';
+        }
+    }
+
+    const details = document.querySelectorAll('.video-accordion details');
+    details.forEach((el) => {
+        new Accordion(el);
+    });
+})();
